5ec91eb998175d4ced30364406c6df5fbbdd4760
Tatusro-Ikawa <bizitems.567@gmail.com>
2025-07-12 14:39:21 +0900

    feat: フロントエンドプロジェクトの初期コミット

---
+++ src/App.tsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import uitoolkit, { CustomizationOptions } from "@zoom/videosdk-ui-toolkit";
+import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
+import "./App.css";
+
+function App() {
+  let sessionContainer: HTMLDivElement | null = null;
+  // set your auth endpoint here
+  // a sample is available here: https://github.com/zoom/videosdk-auth-endpoint-sample
+  const authEndpoint = "https://zoom-auth-server.vercel.app"; // Vercel deployed auth server
+  const config: CustomizationOptions = {
+    videoSDKJWT: "",
+    sessionName: "SessionA",
+    userName: "UserA",
+    sessionPasscode: "abc123",
+    featuresOptions: {
+      preview: {
+        enable: true,
+      },
+      virtualBackground: {
+        enable: true,
+        virtualBackgrounds: [
+          {
+            url: "https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop",
+          },
+        ],
+      },
+    },
+  };
+  const [role, setRole] = useState(1); // 1: ホスト, 0: ゲスト
+
+  function getVideoSDKJWT() {
+    sessionContainer = document.getElementById(
+      "sessionContainer"
+    ) as HTMLDivElement;
+    document.getElementById("join-flow")!.style.display = "none";
+    fetch(authEndpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        sessionName: config.sessionName,
+        role: role,
+        videoWebRtcMode: 1,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.signature) {
+          console.log(data.signature);
+          config.videoSDKJWT = data.signature;
+          joinSession();
+        } else {
+          console.log(data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function joinSession() {
+    console.log(config);
+    if (sessionContainer) {
+      uitoolkit.joinSession(sessionContainer, config);
+      sessionContainer && uitoolkit.onSessionClosed(sessionClosed);
+      uitoolkit.onSessionDestroyed(sessionDestroyed);
+    }
+  }
+
+  const sessionClosed = () => {
+    console.log("session closed");
+    document.getElementById("join-flow")!.style.display = "block";
+  };
+
+  const sessionDestroyed = () => {
+    console.log("session destroyed");
+    uitoolkit.destroy();
+  };
+
+  return (
+    <div className="App">
+      <main>
+        <div id="join-flow">
+          <h1>Zoom Video SDK Sample React</h1>
+          <p>User interface offered by the Video SDK UI Toolkit</p>
+          <div style={{ marginBottom: "20px" }}>
+            <label>
+              <input
+                type="radio"
+                checked={role === 1}
+                onChange={() => setRole(1)}
+              />
+              ホスト (Host)
+            </label>
+            <label style={{ marginLeft: "20px" }}>
+              <input
+                type="radio"
+                checked={role === 0}
+                onChange={() => setRole(0)}
+              />
+              ゲスト (Guest)
+            </label>
+          </div>
+          <button onClick={getVideoSDKJWT}>Join Session</button>
+        </div>
+        <div id="sessionContainer"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
